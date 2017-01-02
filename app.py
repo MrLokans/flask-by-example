5f2d19377ee402b42e15618f46ca3a8f097a0a96
MrLokans <trikster1911@gmail.com>
2017-01-02 08:51:58 +0300

    Added simple view parsing the provided URL.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 
-from flask import Flask
+import requests
+
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -12,9 +14,29 @@ db = SQLAlchemy(app)
 from models import Result
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hello World'
+    errors = []
+    results = {}
+    if request.method == 'POST':
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+        except KeyError:
+            app.logger.warn("Error reading URL from request form.")
+            errors.append('No URL submitted')
+        except requests.exceptions.InvalidURL:
+            # TODO: sanitize user input
+            app.logger.exception("Invalid URL supplied: {}.".format(url))
+            errors.append('Invalid URL supplied')
+        except requests.exceptions.ConnectionError:
+            app.logger.exception("Inexistent host supplied: {}.".format(url))
+            errors.append('Probably host does not exist.')
+        except:
+            app.logger.exception("Unknown error scraping website: {}."
+                                 .format(url))
+            errors.append('Invalid URL supplied')
+    return render_template('index.html', errors=errors, results=results)
 
 
 if __name__ == '__main__':
